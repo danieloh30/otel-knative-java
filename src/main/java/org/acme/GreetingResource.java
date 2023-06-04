a4569ee30563e5780e51c3823c3f828e72f2e857
Daniel Oh <doh@redhat.com>
2023-06-03 20:26:53 -0400

    Update Otel and Quarkus 3.1.0 and Add JDBC telemetry

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,7 +1,10 @@
 package org.acme;
 
+import java.util.List;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -17,26 +20,35 @@ public class GreetingResource {
     String username;
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/person")
+    public List<Person> listAll() {
+        LOG.info("List all person data");
+        return Person.listAll();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/person/{id}")
+    public Person findById(@PathParam("id") Long id) {
+        LOG.info("Find a person data");
+        return Person.findById(id);
+    }
+
+
+    @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        LOG.info("Hello from RESTEasy Reactive");
-        return "Hello from RESTEasy Reactive";
+        LOG.info("Distributed Tracing Integration with OpenTelemetry and Knative!");
+        return "Distributed Tracing Integration with OpenTelemetry and Knative!";
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("greeting")
     public String greeting() {
-        LOG.info("Welcome KCD Italy 2023, " + username);
-        return "Welcome KCD Italy 2023, " + username;
-    }
-
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("otel")
-    public String hola() {
-        LOG.info("Distributed Tracing integrating with Quarkus, Knative and Otel!!");
-        return "Distributed Tracing integrating with Quarkus, Knative and Otel!!";
+        LOG.info("Welcome " + username);
+        return "Welcome " + username;
     }
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into person values (1,'Dan','BOS');
+insert into person values (2,'Jen','NYC');
+insert into person values (3,'Tim','SFO');
